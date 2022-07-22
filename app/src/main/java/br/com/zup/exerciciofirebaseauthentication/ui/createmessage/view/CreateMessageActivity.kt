8e3698053fc82da2a538927593de7b66c719196e
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-07-22 12:04:35 -0300

    Adicionar botão de voltar

--- app/src/main/java/br/com/zup/exerciciofirebaseauthentication/ui/createmessage/view/CreateMessageActivity.kt
+++ app/src/main/java/br/com/zup/exerciciofirebaseauthentication/ui/createmessage/view/CreateMessageActivity.kt
@@ -3,6 +3,9 @@ package br.com.zup.exerciciofirebaseauthentication.ui.createmessage.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
@@ -15,6 +18,7 @@ import br.com.zup.exerciciofirebaseauthentication.ui.createmessage.viewmodel.Cre
 import br.com.zup.exerciciofirebaseauthentication.ui.favorite.view.FavoriteActivity
 import br.com.zup.exerciciofirebaseauthentication.ui.favorite.view.FavoriteAdapter
 import br.com.zup.exerciciofirebaseauthentication.ui.home.viewmodel.HomeViewModel
+import br.com.zup.exerciciofirebaseauthentication.ui.login.view.LoginActivity
 
 class CreateMessageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateMessageBinding
@@ -31,6 +35,7 @@ class CreateMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateMessageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setRecyclerView()
         goToFavoriteList()
     }
@@ -59,5 +64,13 @@ class CreateMessageActivity : AppCompatActivity() {
         binding.floatingActionButton.setOnClickListener {
             startActivity(Intent(this, FavoriteActivity::class.java))
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/br/com/zup/exerciciofirebaseauthentication/ui/favorite/view/FavoriteActivity.kt
+++ app/src/main/java/br/com/zup/exerciciofirebaseauthentication/ui/favorite/view/FavoriteActivity.kt
@@ -1,12 +1,22 @@
 package br.com.zup.exerciciofirebaseauthentication.ui.favorite.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import br.com.zup.exerciciofirebaseauthentication.R
 
 class FavoriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
